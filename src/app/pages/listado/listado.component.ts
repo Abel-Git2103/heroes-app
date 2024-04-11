593b4fde5559e7aff14a67380839b995d0311083
Abel-Git2103 <abel_jornet@hotmail.com>
2024-04-11 10:39:58 +0200

    feature: Diseño de listado y ajustes de layout
    Detección de path de la url para detectar que botón queda seleccionado en el topbar y diseño base del grid del listado

--- src/app/pages/listado/listado.component.ts
+++ src/app/pages/listado/listado.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Constants } from 'src/app/shared/models/constants.model';
 
 @Component({
-  selector: 'app-listado',
-  templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+    selector: 'app-listado',
+    templateUrl: './listado.component.html',
+    styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+    /* Propiedades del listado */
+    public numberCols: number = 6;
 
+    /* Propiedades constantes */
+    public MEDIABREAKPOINTS = Object.freeze(Constants.mediaBreakpoints);
+
+    ngOnInit(): void {
+        this.checkNumberCols();
+    }
+
+    /* Recupera el tamaño de la ventana para poder asignar el número de columnas que se muestran en pantalla para hacerla responsive */
+    checkNumberCols() {
+        let windowSize: number = window.innerWidth;
+
+        if (windowSize < this.MEDIABREAKPOINTS.sm) {
+            this.numberCols = 1;
+        } else if (windowSize < this.MEDIABREAKPOINTS.md) {
+            this.numberCols = 2;
+        } else if (windowSize < this.MEDIABREAKPOINTS.lg) {
+            this.numberCols = 3;
+        } else if (windowSize < this.MEDIABREAKPOINTS.xl) {
+            this.numberCols = 4;
+        } else {
+            this.numberCols = 6;
+        }
+    }
 }
---
+++ src/app/shared/models/constants.model.ts
@@ -0,0 +1,15 @@
+export const Constants = {
+    appUrls: {
+        listado: '/app/listado',
+        busqueda: '/app/busqueda',
+        nuevoHeroe: '/app/nuevo-heroe',
+        editarHeroe: '/app/editar-heroe/',
+        heroe: '/app/heroe/'
+    },
+    mediaBreakpoints: {
+        sm: 540,
+        md: 768,
+        lg: 960,
+        xl: 1140
+    }
+};
