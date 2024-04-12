88b1ff6cdbd7c2c888af4043ab74aa996e390198
Abel-Git2103 <abel_jornet@hotmail.com>
2024-04-12 12:17:52 +0200

    feature: Funcionalidad botones card y dialog confirmación

--- src/app/pages/listado/listado.component.ts
+++ src/app/pages/listado/listado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogoConfirmacionComponent } from 'src/app/components/dialogo-confirmacion/dialogo-confirmacion.component';
 import { ListadoService } from 'src/app/services/listado.service';
 import { Constants } from 'src/app/shared/models/constants.model';
 import { Heroe } from 'src/app/shared/models/heroe.model';
@@ -12,15 +14,22 @@ export class ListadoComponent implements OnInit {
     /* Propiedades del listado */
     public listadoHeroes: Heroe[] = [];
 
-    constructor(private listadoService: ListadoService) {}
+    constructor(private listadoService: ListadoService, private dialog: MatDialog) {}
 
     ngOnInit(): void {
         this.getHeroesList();
     }
 
     /* PUBLIC METHODS */
+    /* Al hacer click en el botón eliminar, se llama para abrir el diálogo de confirmación */
+    public confirmDialogDelete() {
+        this.dialog.open(DialogoConfirmacionComponent, {
+            width: '450px'
+        });
+    }
 
     /* PRIVATE METHODS */
+    /* Obtiene el listado de héroes del servicio */
     private getHeroesList() {
         this.listadoService.obtenerListadoHeroes().subscribe({
             next: (listado: Heroe[]) => {
